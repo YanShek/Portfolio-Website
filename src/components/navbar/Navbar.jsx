dbd11fef8a23641c0e112a7973365b478e3a1694
YanShek <yanshek1123@gmail.com>
2024-03-24 15:34:49 -0400

    Main Page and Navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.scss"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            {/* Sidebar */}
+            <div className="wrapper">
+                <span>Smth</span>
+                <div className="social">
+                    <a href=""></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
