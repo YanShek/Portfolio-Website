0e70a2ab3d9633fb1716ce5af1f50aa61d8e0e8a
Andrei Bituleanu <andrei.bituleanu@yahoo.com>
2025-01-01 19:35:04 -0500

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,22 @@ import Navbar from "./components/navbar/Navbar";
 import Portfolio from "./components/portfolio/Portfolio";
 
 const App = () => {
-  return <div>
-    <section id="Home">
-      <Navbar displayText={"Yan"}/>
-      <Hero/>
-    </section>
-    <section id="About"><Navbar displayText={"About"}/><About/></section>  
-    <section id="Portfolio"><Portfolio/></section>
-    <section id="Contact">Contact</section> 
-  
-  </div>;
-
+	return (
+		<div className="app">
+			<section id="Home">
+				<Navbar displayText={"Yan"} />
+				<Hero />
+			</section>
+			<section id="About">
+				<Navbar displayText={"About"} />
+				<About />
+			</section>
+			<section id="Portfolio">
+				<Portfolio />
+			</section>
+			<section id="Contact">Contact</section>
+		</div>
+	);
 };
 
 export default App;
